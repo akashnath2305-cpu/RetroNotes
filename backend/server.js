393e687d6d62469ca319f4231ba2824a19cb323e
akashnath2305-cpu <akashnath2305@gmail.com>
2026-06-10 12:47:37 +0530

    Updates

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const pdfParse = require('pdf-parse');
